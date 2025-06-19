aa03242afd1edd8a22907b149379faf8ae1f2c83
joe-gomez <joe.gomez@lis.ac.uk>
2025-06-19 14:01:02 +0200

    Update highlight.py

--- highlight.py
+++ highlight.py
@@ -1,6 +1,25 @@
 import re
+import spacy
 
-def highlight_individualising_language(text, phrases):
-    pattern = r'(' + '|'.join(re.escape(p) for p in phrases) + r')'
-    highlighted = re.sub(pattern, r'<span class="highlight">\1</span>', text, flags=re.IGNORECASE)
-    return highlighted
+# Load spaCy English model
+nlp = spacy.load("en_core_web_sm")
+
+# Define phrases as simple keywords (can be extended later)
+individualising_keywords = [
+    "you should", "your responsibility", "individual choice", "personal duty",
+    "you must", "on you", "each person", "it's up to you", "do your part"
+]
+
+def highlight_individualising_language(text):
+    doc = nlp(text)
+    highlighted_text = text
+
+    for phrase in individualising_keywords:
+        highlighted_text = re.sub(
+            re.escape(phrase), 
+            f'<span class="highlight">{phrase}</span>', 
+            highlighted_text, 
+            flags=re.IGNORECASE
+        )
+
+    return highlighted_text
