d72da1b317e9f403a4102da6a4f9b16979bb1757
joe-gomez <joe.gomez@lis.ac.uk>
2025-06-19 14:47:53 +0200

    Update highlight.py

--- highlight.py
+++ highlight.py
@@ -1,10 +1,11 @@
+# highlight.py
 import re
 
 def highlight_individualising_language(text):
-    individualising_phrases = [
+    phrases = [
         "you should", "your responsibility", "individual choice", "personal duty",
         "you must", "on you", "each person", "it's up to you", "do your part"
     ]
-    pattern = r'(' + '|'.join(re.escape(p) for p in individualising_phrases) + r')'
+    pattern = r'(' + '|'.join(re.escape(p) for p in phrases) + r')'
     highlighted = re.sub(pattern, r'<span class="highlight">\1</span>', text, flags=re.IGNORECASE)
     return highlighted
